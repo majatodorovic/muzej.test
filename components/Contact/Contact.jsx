8ae675612b7e98dbc361fefe0681e63946d6c33a
Marija Todorovic <maja.todorovic2111@gmail.com>
2025-08-22 09:32:23 +0200

    Test

--- components/Contact/Contact.jsx
+++ components/Contact/Contact.jsx
@@ -1,5 +1,4 @@
 "use client";
-
 import { useEffect, useState } from "react";
 import {
   GoogleReCaptchaProvider as Provider,
@@ -15,27 +14,46 @@ import SvgInput from "../svg/Paths/SvgInput";
 import SvgTextArea from "../svg/Paths/SvgTextArea";
 
 const ContactForm = ({ defaultMessage }) => {
-  const { executeRecaptcha } = useGoogleReCaptcha();
   const [errors, setErrors] = useState([]);
   const [loading, setLoading] = useState(false);
-
-  const [textOptions] = useState([
-    { id: 1, buttonText: "Nađen markirani slepi miš", messageText: "Nađen markirani slepi miš. " },
-    { id: 2, buttonText: "Nađena markirana ptica", messageText: "Nađena markirana ptica. " },
-  ]);
 
   const searchParams = useSearchParams();
   const pathname = usePathname();
   const isContactPage = pathname === "/kontakt";
 
+  const [textOptions] = useState([
+    {
+      id: 1,
+      buttonText: "Nađen markirani slepi miš",
+      messageText: "Nađen markirani slepi miš. ",
+    },
+    {
+      id: 2,
+      buttonText: "Nađena markirana ptica",
+      messageText: "Nađena markirana ptica. ",
+    },
+  ]);
+
   const [contactPageOptions] = useState([
-    { id: 1, buttonText: "Prirodnjački muzej", value: "contact_page_prirodnjacki_muzej" },
-    { id: 2, buttonText: "Galerija Prirodnjačkog muzeja", value: "contact_page_galerija_prirodnjackog_muzeja" },
+    {
+      id: 1,
+      buttonText: "Prirodnjački muzej",
+      value: "contact_page_prirodnjacki_muzej",
+    },
+    {
+      id: 2,
+      buttonText: "Galerija Prirodnjačkog muzeja",
+      value: "contact_page_galerija_prirodnjackog_muzeja",
+    },
   ]);
 
   const [contactPage, setContactPage] = useState(
-    isContactPage ? contactPageOptions[0].value : "contact_page_centar_za_markiranje_zivotinja"
+    isContactPage
+      ? contactPageOptions[0].value
+      : "contact_page_centar_za_markiranje_zivotinja"
   );
+
+  const requiredFields = ["customer_name", "email", "message"];
 
   const [formData, setFormData] = useState({
     page_section: contactPage,
@@ -43,192 +61,244 @@ const ContactForm = ({ defaultMessage }) => {
     phone: "",
     email: "",
     message: defaultMessage || "",
-    gcaptcha: "",
   });
+
+  const { executeRecaptcha } = useGoogleReCaptcha();
 
   useEffect(() => {
     const productId = searchParams.get("id");
     if (productId) {
-      setFormData((prev) => ({ ...prev, message: `Potrebne informacije za proizvod ${productId}` }));
+      setFormData((prev) => ({
+        ...prev,
+        message: `Potrebne informacije za proizvod ${productId}`,
+      }));
     }
   }, []);
 
   useEffect(() => {
-    setFormData((prev) => ({ ...prev, page_section: contactPage }));
+    setFormData((prev) => ({
+      ...prev,
+      page_section: contactPage,
+    }));
   }, [contactPage]);
 
-  const requiredFields = ["customer_name", "email", "message"];
-
   const handleChange = ({ target }) => {
-    setErrors((prev) => prev.filter((err) => err !== target.name));
-    setFormData((prev) => ({ ...prev, [target.name]: target.value }));
+    const updatedErrors = errors.filter((error) => error !== target.name);
+    setErrors(updatedErrors);
+
+    setFormData((prev) => ({
+      ...prev,
+      [target.name]: target.value,
+    }));
   };
 
   const handleSetPredefinedText = (text) => {
-    setFormData((prev) => ({ ...prev, message: text }));
+    setFormData((prev) => ({
+      ...prev,
+      message: text,
+    }));
   };
 
   const handleSubmit = async (e) => {
     e.preventDefault();
     setLoading(true);
 
-    // Validacija
-    const errorsArr = requiredFields.filter((field) => !formData[field]);
-    setErrors(errorsArr);
-    if (errorsArr.length > 0) {
+    const currentErrors = [];
+    requiredFields.forEach((field) => {
+      if (!formData[field]) {
+        currentErrors.push(field);
+      }
+    });
+
+    setErrors(currentErrors);
+    if (currentErrors.length > 0) {
       setLoading(false);
       return;
     }
 
     if (!executeRecaptcha) {
-      toast.error("reCAPTCHA nije spreman");
+      toast.error("reCAPTCHA nije spremna. Pokušajte ponovo.");
       setLoading(false);
       return;
     }
 
     try {
-      // UVEK uzmi novi token za svaki submit
       const token = await executeRecaptcha("contact_form");
 
-      const response = await POST(
+      const res = await POST(
         `/contact/contact_page?form_section=${contactPage}`,
-        { ...formData, gcaptcha: token, customer_name: formData.customer_name.trim() }
+        {
+          ...formData,
+          customer_name: `${formData.customer_name} `,
+          gcaptcha: token,
+        }
       );
 
-      if (response?.code === 200) {
-        toast.success("Uspešno ste poslali poruku!", { position: "top-center", autoClose: 2000 });
+      if (res?.code === 200) {
+        toast.success("Uspešno ste poslali poruku!", {
+          position: "top-center",
+          autoClose: 2000,
+        });
+
         setFormData({
           page_section: contactPage,
           customer_name: "",
           phone: "",
           email: "",
           message: "",
-          gcaptcha: "",
         });
+
+        // Resetovanje tokena posle uspešnog slanja
+        if (executeRecaptcha) {
+          await executeRecaptcha("contact_form");
+        }
       } else {
-        toast.error("Došlo je do greške! Pokušajte ponovo.", { position: "top-center", autoClose: 2000 });
+        toast.error("Došlo je do greške! Pokušajte ponovo.", {
+          position: "top-center",
+          autoClose: 2000,
+        });
       }
     } catch (error) {
-      toast.error("Greška pri slanju poruke.", { position: "top-center", autoClose: 2000 });
-    } finally {
-      setLoading(false);
+      toast.error("Greška prilikom slanja poruke.");
     }
+
+    setLoading(false);
   };
 
   return (
-    <div className="flex items-center gap-10 max-md:flex-col">
-      <form className="flex w-full flex-1 flex-col gap-6">
-        <div className="w-1/2 text-lg font-light max-lg:w-full">
-          Ako pronađete pticu ili slepog miša sa prstenom javite se nacionalnom Centru za markiranje životinja u Prirodnjačkom muzeju.
-        </div>
-
+    <form className="flex w-full flex-1 flex-col gap-6">
+      <div className="w-1/2 text-lg font-light max-lg:w-full">
+        Ako pronađete pticu ili slepog miša sa prstenom javite se
+        nacionalnom Centru za markiranje životinja u Prirodnjačkom muzeju.
+      </div>
+      <div className="relative">
+        <SvgInput className="w-full 2xl:w-2/3" fill="#fff" />
+        <input
+          required
+          type="text"
+          value={formData.customer_name}
+          name="customer_name"
+          onChange={handleChange}
+          placeholder="Ime i prezime"
+          className="mainInput2"
+        />
+      </div>
+      {isContactPage && (
         <div className="relative">
           <SvgInput className="w-full 2xl:w-2/3" fill="#fff" />
           <input
             required
             type="text"
-            value={formData.customer_name}
-            name="customer_name"
+            value={formData.phone}
+            name="phone"
             onChange={handleChange}
-            placeholder="Ime i prezime"
-            className="mainInput2"
-          />
-        </div>
-
-        {isContactPage && (
-          <div className="relative">
-            <SvgInput className="w-full 2xl:w-2/3" fill="#fff" />
-            <input
-              type="text"
-              value={formData.phone}
-              name="phone"
-              onChange={handleChange}
-              placeholder="Telefon"
-              className="mainInput2 w-full 2xl:w-2/3"
-            />
-          </div>
-        )}
-
-        <div className="relative">
-          <SvgInput className="w-full 2xl:w-2/3" fill="#fff" />
-          <input
-            required
-            type="email"
-            value={formData.email}
-            name="email"
-            onChange={handleChange}
-            placeholder="Adresa e-pošte"
+            placeholder="Telefon"
             className="mainInput2 w-full 2xl:w-2/3"
           />
         </div>
-
-        <div className="relative">
-          <SvgTextArea className="w-full 2xl:w-2/3" fill="#fff" />
-          <textarea
-            required
-            name="message"
-            rows={6}
-            value={formData.message}
-            onChange={handleChange}
-            placeholder="Poruka"
-            className="mainInput2 w-full py-10 2xl:w-2/3"
-          />
-        </div>
-
-        {isContactPage ? (
-          <div className="flex flex-col gap-4">
-            {contactPageOptions.map((option) => (
-              <button
-                key={option.id}
-                onClick={() => setContactPage(option.value)}
-                type="button"
-                className="relative w-[320px] transition-all hover:opacity-80"
-              >
-                <SvgButtonOne
-                  className="mx-auto h-[52px] w-[320px]"
-                  fill={contactPage === option.value ? "#224e35" : "transparent"}
-                />
-                <div className={`buttonText w-[320px] ${contactPage === option.value ? "" : "!text-primary"}`}>
-                  {option.buttonText}
-                </div>
-              </button>
-            ))}
-          </div>
-        ) : (
-          <div className="flex w-[300px] flex-col gap-4">
-            {textOptions.map((option) => (
-              <button
-                key={option.id}
-                onClick={() => handleSetPredefinedText(option.messageText)}
-                type="button"
-                className="relative w-[300px] transition-all hover:opacity-80"
-              >
-                <SvgButtonOne className="mx-auto h-[52px] w-[300px]" fill="transparent" />
-                <div className="buttonText w-[300px] !text-primary">{option.buttonText}</div>
-              </button>
-            ))}
-          </div>
-        )}
-
-        <div className="flex w-full 2xl:w-2/3">
-          <button onClick={handleSubmit} className="relative w-[250px]" disabled={loading}>
-            <SvgButtonOne className="mx-auto h-[52px] w-[250px]" />
-            <div className="buttonText">{loading ? <i className="fa fa-spinner fa-spin"></i> : "Pošalji"}</div>
-          </button>
-        </div>
-      </form>
-
-      <div className="flex-1">
-        <SvgWithImage image="/images/museum.png" alt="museum" />
+      )}
+      <div className="relative">
+        <SvgInput className="w-full 2xl:w-2/3" fill="#fff" />
+        <input
+          required
+          type="email"
+          value={formData.email}
+          name="email"
+          onChange={handleChange}
+          placeholder="Adresa e-pošte"
+          className="mainInput2 w-full 2xl:w-2/3"
+        />
       </div>
-    </div>
+      <div className="relative">
+        <SvgTextArea className="w-full 2xl:w-2/3" fill="#fff" />
+        <textarea
+          required
+          name="message"
+          rows={6}
+          value={formData.message}
+          onChange={handleChange}
+          placeholder="Poruka"
+          className="mainInput2 w-full py-10 2xl:w-2/3"
+        ></textarea>
+      </div>
+
+      {isContactPage ? (
+        <div className="flex flex-col gap-4">
+          {contactPageOptions.map((option) => (
+            <button
+              key={option.id}
+              onClick={() => setContactPage(option.value)}
+              type="button"
+              className="relative w-[320px] transition-all hover:opacity-80"
+            >
+              <SvgButtonOne
+                className="mx-auto h-[52px] w-[320px]"
+                fill={
+                  contactPage === option.value ? "#224e35" : "transparent"
+                }
+              />
+              <div
+                className={`buttonText w-[320px] ${
+                  contactPage === option.value ? "" : "!text-primary"
+                }`}
+              >
+                {option.buttonText}
+              </div>
+            </button>
+          ))}
+        </div>
+      ) : (
+        <div className="flex w-[300px] flex-col gap-4">
+          {textOptions.map((option) => (
+            <button
+              key={option.id}
+              onClick={() => handleSetPredefinedText(option.messageText)}
+              type="button"
+              className="relative w-[300px] transition-all hover:opacity-80"
+            >
+              <SvgButtonOne
+                className="mx-auto h-[52px] w-[300px]"
+                fill="transparent"
+              />
+              <div className="buttonText w-[300px] !text-primary">
+                {option.buttonText}
+              </div>
+            </button>
+          ))}
+        </div>
+      )}
+
+      <div className="flex w-full 2xl:w-2/3">
+        <button
+          onClick={handleSubmit}
+          className="relative w-[250px]"
+          disabled={loading}
+        >
+          <SvgButtonOne className="mx-auto h-[52px] w-[250px]" />
+          <div className="buttonText">
+            {loading ? (
+              <i className="fa fa-spinner fa-spin"></i>
+            ) : (
+              "Pošalji"
+            )}
+          </div>
+        </button>
+      </div>
+    </form>
   );
 };
 
-const Contact = ({ defaultMessage }) => (
-  <Provider reCaptchaKey={process.env.CAPTCHAKEY}>
-    <ContactForm defaultMessage={defaultMessage} />
-  </Provider>
-);
+const Contact = (props) => {
+  return (
+    <Provider reCaptchaKey={process.env.CAPTCHAKEY}>
+      <div className="flex items-center gap-10 max-md:flex-col">
+        <ContactForm {...props} />
+        <div className="flex-1">
+          <SvgWithImage image="/images/museum.png" alt="museum" />
+        </div>
+      </div>
+    </Provider>
+  );
+};
 
 export default Contact;
